44a2dea80925136e087243d24bb170ba6973290d
riggsthe <tessie.riggs@gmail.com>
2015-10-01 13:35:01 -0400

    lines 19-22

--- exercise1.py
+++ exercise1.py
@@ -6,13 +6,17 @@ This module prints the amount of money that Lakshmi has remaining
 after the stock transactions
 
 """
-#tessie's first change
 __author__ = 'Susan Sim'
 __email__ = "ses@drsusansim.org"
 __copyright__ = "2015 Susan Sim"
 __license__ = "MIT License"
 
-#tessie's second change
-#Dusan's first change
+
 money = 1000.00
 print(money)
+
+
+#number_of_shares_bought = 2000
+#price_per_share = 900.00
+# total_cost = (number_of_shares * price_per_share)
+#broker_commission = total_cost * 0.03
